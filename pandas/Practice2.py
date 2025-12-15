6a32248c535542c59a5901bbda863c647ef7a986
neverbelikeabdullah6075 <muhammadabdullahmateen7@gmail.com>
2025-12-16 03:59:26 +0500

    Added more things

--- pandas/Practice2.py
+++ pandas/Practice2.py
@@ -10,10 +10,10 @@ print(df)
 print("Names in the Dataframe:")
 
 print(df['NAME'])
+
 print("ITERATING THROUGH NAMES:")
 for name in df['NAME']:
     print(name)
-
 print("above will print names one by one by remoing the indexes")
 
 print("Filtering Dataframe based on condition like gpas and age:")
@@ -22,3 +22,16 @@ print(filter_df)
 print("Sorting DF in descending order based on GPA:")
 sorted_df=df.sort_values(by='GPA',ascending=False)
 print(sorted_df)
+
+print("Adding new column to DF:")
+df['GRADES']=['A','A+','B+','A']
+print(df)
+
+print("DROPPING COLUMNS:")
+df_no_name = df.drop('AGE',axis=1)  # axis=1 means column
+print("Without AGE column:")
+print(df_no_name)
+
+print("Descriptive statistics of the DataFrame:")
+print(df.describe())
+print(df['GPA'].describe())
